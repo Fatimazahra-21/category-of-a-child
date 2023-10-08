9696e9c56184fd36fa5d1bbefc278a4cd3021762
Fatima zahra majdou <146893864+Fatimazahra-21@users.noreply.github.com>
2023-10-09 01:27:17 +0300

    Create catégory.py

---
+++ catégory.py
@@ -0,0 +1,11 @@
+age = int(input("entrer age de enfant :"))
+if age >= 7 :
+print("la catégorie de enfant estpoussin")
+elif age >= 8 and age <= 9 :
+print("la catégorie de enfant est pupille")
+elif age >= 10 and age <=11 :
+print("lacatégorie de enfant est minime")
+elif age >= 12 :
+print("la catégorie de enfant est cadet")
+else:
+print("la catégorie nexiste pas")
